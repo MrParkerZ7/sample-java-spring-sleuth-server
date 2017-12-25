2383b394d46bead72f70bc62d3b8b5d40aff2f09
Park <neverzero9@gmail.com>
2017-12-25 14:38:34 +0700

    full example

--- src/main/java/com/park/promsawat/spring/sleuth/server/example/springsleuthserver/controller/MotorbikeFactoryController.java
+++ src/main/java/com/park/promsawat/spring/sleuth/server/example/springsleuthserver/controller/MotorbikeFactoryController.java
@@ -2,6 +2,7 @@ package com.park.promsawat.spring.sleuth.server.example.springsleuthserver.contr
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,18 @@ public class MotorbikeFactoryController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MotorbikeFactoryController.class);
 
-    @RequestMapping("/oder/{bike}")
-    public String builMotorBike(@PathVariable("bike") final String bike) {
-        return null;
+    @GetMapping("/order/{bike}")
+    public String buildMotorBike(@PathVariable("bike") final String bike) {
+
+        if (bike.equals("BMW"))
+            return "S1000RR";
+        else if (bike.equals("Kawasaki"))
+            return "ZX10R";
+        else if (bike.equals("Honda"))
+            return "CBR1000RR";
+        else if (bike.equals("Yamaha"))
+            return "R1";
+        else
+            return "No bike for you";
     }
 }
